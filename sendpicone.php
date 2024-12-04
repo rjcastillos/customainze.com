40b8ba797228823e80f1b98ead1afd2878443fab
Ramon Castillo <ramon@rcastillo.net>
2024-12-04 14:29:35 +0100

    Update sendpicone.php
    Blankingout c

--- sendpicone.php
+++ sendpicone.php
@@ -30,12 +30,12 @@ try {
  //   $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->SMTPDebug = SMTP::DEBUG_OFF;
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'mail.madredemisericordia.es';                     //Set the SMTP server to send through
+    $mail->Host       = 'xxxxxx';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'itsupport@madredemisericordia.es';                     //SMTP username
-    $mail->Password   = 'EsperanzayCaridad23';                               //SMTP password
+    $mail->Username   = 'xxxxx';                     //SMTP username
+    $mail->Password   = 'xxxxxxxxxxx';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;             //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->Port       = xxx;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
     $mail->setFrom('itsupport@madredemisericordia.es', 'MDM IT Support');
