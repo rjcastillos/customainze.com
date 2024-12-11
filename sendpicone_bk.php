68e82d6ba4a707722d6e9009f15674e2bdc87571
Ramon Castillo <ramon@rcastillo.net>
2024-12-11 13:27:07 +0100

    adding the .css , .gitignore and taking out sendpi to the build

--- sendpicone_bk.php
+++ sendpicone_bk.php
@@ -17,32 +17,45 @@ require '../emails/src/SMTP.php';
 $mail = new PHPMailer(true);
 $fileToEmail = 'IMG_2424.jpeg';
 $passfilename = $_GET['fileToEmail'];
+$refinvoice = $_GET['proofp'];
+$item1 = $_GET['crystal1'];
+$item2 = $_GET['crystal2'];
+$item3 = $_GET['crystal3'];
+$item4 = $_GET['crystal4'];
 echo "Passed name = $passfilename </br>";
 
 
 try {
-     //Server settings
-     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-     $mail->isSMTP();                                            //Send using SMTP
-     $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
-     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-     $mail->Username   = 'user@example.com';                     //SMTP username
-     $mail->Password   = 'secret';                               //SMTP password
-     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
- 
-     //Recipients
-     $mail->setFrom('from@example.com', 'Mailer');
-     $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-     $mail->addAddress('ellen@example.com');               //Name is optional
-     $mail->addReplyTo('info@example.com', 'Information');
-     $mail->addCC('cc@example.com');
-     $mail->addBCC('bcc@example.com');
+    //Server settings
+ //   $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->SMTPDebug = SMTP::DEBUG_OFF;
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'XXXXX';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'XXXXXX';                     //SMTP username
+    $mail->Password   = 'XXXXXX';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;             //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('xxx@xxx.com', 'MDM IT Support');
+    $mail->addAddress('@hotmail.com', 'K');     //Add a recipient RECEIVER
+    $mail->addAddress('@gmail.com', 'Process Customainze');     //Add a recipient RECEIVER
+    //$mail->addAddress('ramonjoseph.c@gmail.com', 'Process Customainze');     //Add a recipient RECEIVER
+    //$mail->addAddress('ellen@example.com');               //Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    $mail->addBCC('@protonmail.com');
+
+    //Attachments
+    //$mail->addAttachment('../../public_ftp/incoming/IMG_2424.jpeg');         //Add attachments --working line
+    $mail->addAttachment("$passfilename");
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = 'Uploaded picture to process by Customainze';
-    $mail->Body    = 'Please proces file  <b>Thanks from MDM team!</b>';
+    $mail->Body    = "Please process file  <b>Thanks from MDM team!</b><br/> Ref : $refinvoice items : $item1 , $item2 , $item3 , $item4";
     $mail->AltBody = 'In case your email client do not support html , thanks from MDM team';
 
     $mail->send();
